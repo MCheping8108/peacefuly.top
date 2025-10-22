1dc1944125fa5572773644528bdda66803cc15e6
MCheping8108 <liuyiming53@outlook.com>
2025-10-22 23:27:01 +0800

    Site updated: 2025-10-22 23:26:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/04/NAT类型讲解/","2023/10/11/ChatGPT-Next-Web同步教程/","2025/08/04/hello-world/","2025/01/26/搭建原神服务器/","2023/08/22/如何用自定义NPC做一个战斗型机器人/"];function toRandomPost(){
+var posts=["2025/08/04/NAT类型讲解/","2023/10/11/ChatGPT-Next-Web同步教程/","2025/08/04/hello-world/","2025/01/26/搭建原神服务器/","2023/08/22/如何用自定义NPC做一个战斗型机器人/","2025/10/21/Adguard-Home部署/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
